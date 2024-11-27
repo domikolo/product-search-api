ef4960361dd64abadae99dcd3c6a7c5880e5a767
domikolo <dominikoczki@gmail.com>
2024-11-27 23:34:53 +0100

    Fix port and host configuration for Render

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def search_products():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    PORT = int(os.environ.get('PORT', 5000))  # Pobiera port z zmiennej środowiskowej lub ustawia domyślnie 5000
+    app.run(host='0.0.0.0', port=PORT, debug=True)  # Uruchamia aplikację na publicznym adresie IP
